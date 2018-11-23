559c6266012515f5ff40fe125b9ce789e218cc47
Nixon151 <2020heroz12@gmail.com>
2018-11-23 11:32:29 -0500

    Add files via upload

---
+++ sleekload2/js/config.js
@@ -0,0 +1,138 @@
+/*    _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.08
+*/
+
+/*URL for the server logo
+Leave it blank to use text instead
+(Put the files in the images folder)*/
+var l_serverImage = "";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "ArcticNetworks";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = false;
+
+/*Video background
+Avaiable options:
+"aurora.webm"
+"bluefall.webm"
+"bluetilefloor.webm"
+"bluewhitecircles.webm"
+"bokehlens.webm"
+"cleanbokeh.webm"
+"coldnight.webm"
+"colorfulburst.webm"
+"dancingstars.webm"
+"dropsflares.webm"
+"focusorangebokeh.webm"
+"glimmering.webm"
+"glowingrings.webm"
+"greencircles.webm"
+"hues.webm"
+"inthedeep.webm"
+"modernflourish.webm"
+"orangeaura.webm"
+"warminside.webm"
+"yellowgreen.webm"
+
+You can add custom backgrounds
+Just make sure it is at webm VP8 format 
+(Put the files in the backgrounds/videos folder)*/
+var l_background = "glimmering.webm";
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them
+(Put the files in the backgrounds/images folder)*/
+var l_bgImages = [
+	"military1.jpg",
+	"military2.jpg",
+	"military3.jpg",
+	"military4.jpg",
+	"military5.jpg",
+	"military6.jpg",
+	"military7.jpg",
+	"military8.jpg",
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = false;
+
+/*Display actual song's name?*/
+var l_musicDisplay = false;
+
+/*Music playlist
+Place how much .ogg songs you want
+(Put the files in the songs folder)*/
+var l_musicPlaylist = [
+	{youtube: "https://www.youtube.com/watch?v=kUUn88vmUnE", name: "Eye of the Storm"},
+	{youtube: "https://www.youtube.com/watch?v=MFzYXid7tUk", name: "Till I Collapse"},
+	{youtube: "https://www.youtube.com/watch?v=81eKr29eSWY", name: "Remember the Name"},
+
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 15;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = false;
+
+/*Enter your custom messages below*/
+var l_messages = [
+	"What Do You Call a Soldier Who Survived Mustard Gas & Pepper Spray?",
+	"A seasoned veteran.",
+	"How Do You Play Air Force Bingo?",
+	"B-52...F-16...A-10.",
+	"Did You Hear about the Accident at the Army Base?",
+	"A tank ran over a box of popcorn and killed two kernals!",
+	"What Do You Call a Marine with an IQ of 160?",
+	"A platoon!",
+	"What's the Main Mission of the Marines?",
+	"To make sure the Army never get their feet wet.",
+	
+	
+];
+
+/*Random message order?*/
+var l_messagesRandom = false;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
